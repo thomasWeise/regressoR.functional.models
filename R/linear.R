ad55920169577150777fd161deff43464caae3d8
Thomas Weise <tweise@home>
2018-03-25 17:05:58 +0800

    Removed NLS from Estimators, Put Estimators into Functions

--- R/linear.R
+++ R/linear.R
@@ -19,54 +19,57 @@
   return(NULL);
 }
 
+# the linear estimator function
+.linear.estimator <- function(x, y, paramLower=NULL, paramUpper=NULL) {
+  len <- length(x);
+  res <- NULL;
+  if(len>2L){
+    mid <- as.integer(len/2)
+    res <- .linear.from.two.points(mean(x[1:mid]), mean(y[1:mid]),
+                                   mean(x[(mid+1):len]), mean(y[(mid+1):len]));
+  }
+  if((len >= 2L) && (is.null(res))){
+    res <- .linear.from.two.points(x[1], y[1], x[len], y[len]);
+    if(is.null(res)) {
+      res <- .linear.from.two.points(x[1], y[1], x[2], y[2]);
+    }
+  }
+
+  #    if(len > 2) {
+  #      .ignore.errors({
+  #        if(is.null(res)) {
+  #          refined <- stats::nls(y ~ a*x + b, data=list(x=x, y=y));
+  #        } else {
+  #          refined <- stats::nls(y ~ a*x + b, data=list(x=x, y=y), start=list(a=res[2], b=res[1]));
+  #        }
+  #        if(!(is.null(refined))) {
+  #          if(refined$convInfo$isConv) {
+  #            par <- refined$m$getPars();
+  #            nres <- c(par[["b"]], par[["a"]]);
+  #            if(is.finite(nres[1]) && is.finite(nres[2])) {
+  #              return(nres);
+  #            }
+  #          }
+  #        }
+  #      });
+  #    }
+
+  if(!(is.null(res))) {
+    return(res);
+  }
+
+  if(len >= 1L){
+    return(c(mean(y), 0));
+  }
+  return(c(0, 0));
+}
+
 # The internal constant for linear models
 #' @importFrom stats nls
 .linear <- FunctionalModel.new(
   f = function(x, par) par[1] + (par[2] * x),
   paramCount = 2L,
-  estimator = function(x, y, paramLower=NULL, paramUpper=NULL) {
-    len <- length(x);
-    res <- NULL;
-    if(len>2L){
-      mid <- as.integer(len/2)
-      res <- .linear.from.two.points(mean(x[1:mid]), mean(y[1:mid]),
-                                     mean(x[(mid+1):len]), mean(y[(mid+1):len]));
-    }
-    if((len >= 2L) && (is.null(res))){
-      res <- .linear.from.two.points(x[1], y[1], x[len], y[len]);
-      if(is.null(res)) {
-        res <- .linear.from.two.points(x[1], y[1], x[2], y[2]);
-      }
-    }
-
-    if(len > 2) {
-      .ignore.errors({
-        if(is.null(res)) {
-          refined <- stats::nls(y ~ a*x + b, data=list(x=x, y=y));
-        } else {
-          refined <- stats::nls(y ~ a*x + b, data=list(x=x, y=y), start=list(a=res[2], b=res[1]));
-        }
-        if(!(is.null(refined))) {
-          if(refined$convInfo$isConv) {
-            par <- refined$m$getPars();
-            nres <- c(par[["b"]], par[["a"]]);
-            if(is.finite(nres[1]) && is.finite(nres[2])) {
-              return(nres);
-            }
-          }
-        }
-      });
-    }
-
-    if(!(is.null(res))) {
-      return(res);
-    }
-
-    if(len >= 1L){
-      return(c(mean(y), 0));
-    }
-    return(c(0, 0));
-  },
+  estimator = .linear.estimator,
   gradient = function(x, par) c(1, x)
 )
 
--- R/quadratic.R
+++ R/quadratic.R
@@ -17,71 +17,74 @@
   return(NULL);
 }
 
+# The quadratic estimator function
+.quadratic.estimator <- function(x, y, paramLower, paramUpper) {
+  len <- length(x);
+  res <- NULL;
+
+  if(len > 3) {
+    sres<-sapply(X=1:min(100, len-3),
+                 FUN=function(x) {
+                   sample <- sample.int(n=len, size=3);
+                   .quadratic.from.three.points(x[sample[1]], y[sample[1]],
+                                                x[sample[2]], y[sample[2]],
+                                                x[sample[3]], y[sample[3]]);
+                 });
+    if( (!(is.null(sres))) && (length(sres)>0) ) {
+      if(is.null(dim(sres))) {
+        sres <- sapply(sres[-which(sapply(sres, is.null))], rbind);
+      }
+      if( (!(is.null(sres))) && (length(sres)>0) ) {
+        res <- rowMeans(sres);
+        if(!(is.finite(res[1]) && is.finite(res[2]) && is.finite(res[3]))) {
+          res <- NULL;
+        }
+      }
+    }
+  }
+
+  if((len >= 3) && (is.null(res))) {
+    res <- .quadratic.from.three.points(x[1], y[1], x[len/2L+1], y[len/2L+1], x[len], y[len]);
+    if(is.null(res)) {
+      res <- .quadratic.from.three.points(x[1], y[1], x[2], y[2], x[3], y[3]);
+    }
+  }
+
+#  if(len > 3) {
+#    .ignore.errors({
+#      if(is.null(res)) {
+#        start <- list(a=0, b=0, c=0)
+#      } else {
+#        start <- list(a=res[3], b=res[2], c=res[1]);
+#      }
+#      refined <- nls(y ~ a*x*x + b*x + c, data=list(x=x, y=y), start=start);
+#      if(!(is.null(refined))) {
+#        if(refined$convInfo$isConv) {
+#          par <- refined$m$getPars();
+#          nres <- c(par[["c"]], par[["b"]], par[["a"]]);
+#          if(is.finite(nres[1]) && is.finite(nres[2]) && is.finite(nres[3])) {
+#            return(nres);
+#          }
+#        }
+#      }
+#    });
+#}
+
+  if(!(is.null(res))) {
+    return(res);
+  }
+
+  res <- .linear.estimator(x, y);
+  if(is.null(res)) { return(NULL); }
+  return(c(res, 0));
+};
+
 # The internal constant for linear models
 #' @importFrom stats nls
 .quadratic <- FunctionalModel.new(
   f = function(x, par) par[1] + (x * (par[2] + (x * par[3]))),
   paramCount = 3L,
-  estimator = function(x, y, paramLower, paramUpper) {
-    len <- length(x);
-    res <- NULL;
-
-    if(len > 3) {
-      sres<-sapply(X=1:min(100, len-3),
-             FUN=function(x) {
-               sample <- sample.int(n=len, size=3);
-               .quadratic.from.three.points(x[sample[1]], y[sample[1]],
-                                            x[sample[2]], y[sample[2]],
-                                            x[sample[3]], y[sample[3]]);
-             });
-      if( (!(is.null(sres))) && (length(sres)>0) ) {
-        if(is.null(dim(sres))) {
-          sres <- sapply(sres[-which(sapply(sres, is.null))], rbind);
-        }
-        if( (!(is.null(sres))) && (length(sres)>0) ) {
-          res <- rowMeans(sres);
-          if(!(is.finite(res[1]) && is.finite(res[2]) && is.finite(res[3]))) {
-            res <- NULL;
-          }
-        }
-      }
-    }
-
-    if((len >= 3) && (is.null(res))) {
-      res <- .quadratic.from.three.points(x[1], y[1], x[len/2L+1], y[len/2L+1], x[len], y[len]);
-      if(is.null(res)) {
-        res <- .quadratic.from.three.points(x[1], y[1], x[2], y[2], x[3], y[3]);
-      }
-    }
-
-    if(len > 3) {
-      .ignore.errors({
-        if(is.null(res)) {
-          start <- list(a=0, b=0, c=0)
-        } else {
-          start <- list(a=res[3], b=res[2], c=res[1]);
-        }
-        refined <- nls(y ~ a*x*x + b*x + c, data=list(x=x, y=y), start=start);
-        if(!(is.null(refined))) {
-          if(refined$convInfo$isConv) {
-            par <- refined$m$getPars();
-            nres <- c(par[["c"]], par[["b"]], par[["a"]]);
-            if(is.finite(nres[1]) && is.finite(nres[2]) && is.finite(nres[3])) {
-              return(nres);
-            }
-          }
-        }
-      });
-    }
-
-    if(!(is.null(res))) {
-      return(res);
-    }
-
-    res <- FunctionalModel.linear()@estimator(x, y);
-    if(is.null(res)) { return(NULL); }
-    return(c(res, 0));
-  },
+  estimator = .quadratic.estimator,
   gradient=function(x, par) c(1, x, x*x)
 )
 
