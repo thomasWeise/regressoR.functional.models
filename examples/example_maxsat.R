3bb5aa87642161f32591f8790ba7e1a42ba075e4
Thomas Weise <tweise@hfuu.edu.cn>
2018-03-29 05:58:39 +0800

    Better Colors and Comments in Example

--- examples/example_maxsat.R
+++ examples/example_maxsat.R
@@ -10,43 +10,67 @@ data.y <- data[[3]];
 # make the default plot
 plot(log(data.x), data.y);
 
-# then we load the models and assign colors to them
-models  <- regressoR.functional.models::FunctionalModel.all();
-count   <- length(models);
-colors  <- c("red", "blue", "green", "orange", "darkgreen", "violet",
-             "cyan", "gold", "darkblue", "darkred", "darkmagenta",
-             "aquamarine", "gray", "tan", "darkgray");
-if(length(colors) < count) {
-  colors <- unique(unlist(c(colors, rainbow(count))));
-  if(length(colors) > count) {
-    colors <- colors[1:count];
-  }
+# then we load the models
+models <- regressoR.functional.models::FunctionalModel.all();
+names  <- sapply(models, as.character);
+count  <- length(models);
+
+# now we assign colors
+# we first make a normal list of colors and create darker versions as needed
+
+# source: https://gist.github.com/Jfortin1/72ef064469d1703c6b30
+darken <- function(color, factor=1.7){
+  col <- col2rgb(color)
+  col <- col/factor
+  col <- rgb(t(col), maxColorValue=255)
+  col
+}
+
+# the original color vector
+colors.orig  <- c("red", "blue", "green", "orange",  "violet",
+                  "cyan", "gold", "gray", "darkviolet");
+colors  <- colors.orig;
+# if we do not have enough colors, synthesize more
+while(length(colors) < count) {
+  colors <- unique(unlist(c(colors, sapply(colors.orig, darken))));
 }
 if(length(colors) > count) { colors <- colors[1:count]; }
-names <- sapply(models, as.character);
+# OK, now we have exacty the right number of colors
 
+# add a legend to the plot
 legend("topright",
         text.col=as.vector(colors),
         legend=names);
 
-# prepare the metric
+# prepare the quality metric
 metric    <- regressoR.quality::RegressionQualityMetric.default(data.x, data.y);
+
+# we will keep running statistics about the progress, which initially are NULL
 qualities <- NULL;
 times     <- NULL;
 
-# add the lines
+# we will do at most 100 iterations ... this will take a long time
 for(i in 1L:100L) {
-  results <- lapply(X=models, FUN=function(m) {
-      time <- system.time(result <- regressoR.functional::FunctionalModel.fit(metric, m))[3];
+
+  # fit all the models, while measuring the required time, and also plot the lines
+  results <- lapply(X=1:count, FUN=function(m) {
+      time <- system.time(result <- regressoR.functional::FunctionalModel.fit(metric, models[[m]]))[3];
+
+      # plot the lines
+      if(!(is.null(result))) {
+        lines(log(data.x), result@f(data.x), col=colors[[m]]);
+      }
+
       return(list(time=time, result=result));
     });
 
-  # plot the lines
-  for(j in 1:count) {
-    if(!(is.null(results[[j]]$result))) {
-      lines(log(data.x), results[[j]]$result@f(data.x), col=colors[[j]]);
-    }
-  }
+
+# line plottig now already done inside lapply
+#  for(j in 1:count) {
+#    if(!(is.null(results[[j]]$result))) {
+#      lines(log(data.x), results[[j]]$result@f(data.x), col=colors[[j]]);
+#    }
+#  }
 
   # update qualities list
   .temp   <- vapply(X=results,
@@ -61,6 +85,8 @@ for(i in 1L:100L) {
                   FUN.VALUE = +Inf);
   if(is.null(times)) { times <- matrix(.temp, nrow=1, ncol=count); }
   else               { times <- rbind(times, .temp); }
+
+  # print the state after the iteration
 
   cat("\n######## Iteration ", i, "########\n");
 
