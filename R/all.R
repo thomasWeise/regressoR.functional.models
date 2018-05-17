58e6accf974e4db723400e81c1b7ef567438c25b
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-17 08:08:28 +0800

    Removed MLP Models from Standard Model Set, because they do not Generalize Well

--- R/all.R
+++ R/all.R
@@ -1,3 +1,4 @@
+#' @include constant.R
 #' @include linear.R
 #' @include quadratic.R
 #' @include cubic.R
@@ -5,10 +6,10 @@
 #' @include logistic.R
 #' @include gompertz.R
 #' @include expLogLinear.R
-#' @include mlpDefault.R
 #'
 # the internal variable for default modes
-.all <- unlist(list(FunctionalModel.linear(),
+.all <- unlist(list(FunctionalModel.constant(),
+                    FunctionalModel.linear(),
                     FunctionalModel.quadratic(),
                     FunctionalModel.cubic(),
                     FunctionalModel.exponentialDecay.1(),
@@ -18,12 +19,14 @@
                     FunctionalModel.gompertz.1(),
                     FunctionalModel.gompertz.2(),
                     FunctionalModel.expLogLinear.1(),
-                    FunctionalModel.expLogLinear.2(),
-                    FunctionalModel.mlp.default()))
+                    FunctionalModel.expLogLinear.2()))
 
 #' @title Obtain a List with the All Functional Models Provided by this Library
 #' @description This function returns a list with the all the models provided by
-#'   this library.
+#'   this library. Currently, these comprise standard constant, linear,
+#'   quadratic, and cubic models as well as several sigmoid-style models such as
+#'   exponential decay, logistic models, the Gompertz model, and an exponential
+#'   log-linear model.
 #' @return a list with all models provided by this library.
 #' @export FunctionalModel.all
 #' @seealso FunctionalModel.new
